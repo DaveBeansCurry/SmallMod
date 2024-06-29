bc4d64159d25238d940e39c1c124c6bc639a8b88
dpere <dpere@LAPTOP-DBCS5SCI>
2024-06-29 15:55:20 +1000

    Add project files.

---
+++ SmallMod/RenameNode.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Hacknet;
+using Pathfinder.Action;
+using Pathfinder.Meta.Load;
+using Pathfinder.Util;
+
+namespace RenameNode
+{
+    // Starts Custom Tracer, called Eulogy
+    [Action("RenameNode")]
+    public class RenameNode : DelayablePathfinderAction
+    {
+        [XMLStorage]
+        public string compId;
+
+        [XMLStorage]
+        public string name;
+
+        // Action Trigger
+        public override void Trigger(OS os)
+        {
+            if (string.IsNullOrEmpty(compId) || string.IsNullOrEmpty(name))
+            {
+                os.write("Error: compId or name is null or empty.");
+                return;
+            }
+
+            Computer comp = ComputerLookup.FindById(compId);
+            if (comp == null)
+            {
+                os.write($"Error: No computer found with id {compId}.");
+                return;
+            }
+
+            comp.name = name;
+            os.write($"Computer with id {compId} renamed to {name}.");
+        }
+    }
+}
